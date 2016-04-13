7e473bd0be2626d397a3957d827f4e40a606c43f
Harkirat Singh <harkirat96@gmail.com>
2016-04-13 18:01:52 +0530

    added db interactions

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 
+var config = require('./config/config.json');
+
+var mysql = require('mysql');
+var connection = mysql.createConnection(config	);
+connection.connect();
+//connection.query('UPDATE games SET active = \'false\',winner = \' ' +'draw'+'\',looser = \' '+'draw'+'\' WHERE `games`.`active` = \''+'active'+'\';');
+
+
 app.set('port',(process.env.PORT||3000));
 
 http.listen (app.get('port'),function(){
@@ -26,3 +34,20 @@ app.get( '/', function(req, res)
 	res.render('index',{name : "anshul"});	
 });
 
+app.get('/catalogue', function (req, res)
+{
+	console.log("dasdasads");
+	connection.query('SELECT * FROM items',function(err1, rows, feild) {
+		console.log(rows);
+		res.render('catalogue',{items : rows});
+	});
+});
+
+
+app.get('/items/:id', function (req, res) 
+{
+	console.log(req.params.id);
+	connection.query('SELECT * FROM items WHERE `items`.`id` = '+req.params.id,function(err1, rows, feild) {
+		res.render('item',{item : rows});
+	});
+});
